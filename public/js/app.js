7a25308b9c31038b8de4638adfe6c17ae0db25c9
matukai <matt.tanji@gmail.com>
2018-01-18 12:04:35 -1000

    nav working

--- public/js/app.js
+++ public/js/app.js
@@ -1,28 +1,50 @@
-// default
+// default topic hawaii
 let topic = 'hawaii';
+redditApi(topic);
+
+
 let parentElement = document.getElementById('contentContainer');
 
-let topicHawaii = 'hawaii';
-document.getElementById('subHawaii').addEventListener('click', redditApi(topicHawaii));
 
-let photoShopBattles = 'photoshopbattles';
-document.getElementById('subPhotoShopBattles').addEventListener('click', redditApi(photoShopBattles));
+document.getElementById('subHawaii').addEventListener('click', function (event) {
+  event.preventDefault();
+  let topicHawaii = 'hawaii';
+  console.log('hi')
+  redditApi(topicHawaii)
+})
 
-let technology = 'technology';
-document.getElementById('technology').addEventListener('click', redditApi(technology));
+document.getElementById('subPhotoShopBattles').addEventListener('click', function () {
+  let photoShopBattles = 'photoshopbattles';
+  console.log('fetch ps')
+  redditApi(photoShopBattles)
+});
 
+document.getElementById('technology').addEventListener('click', function () {
+  console.log('fetch tech');
+  let tech = 'technology';
+  redditApi(tech)
+});
+
+document.getElementById('random').addEventListener('click', randomSelect);
+
+function randomSelect() {
+  let arr = ['beamazed', 'confession', 'TalesFromYourServer', 'thettruthishere', 'Glitch_in_the_Matrix'];
+  let index = Math.floor(Math.random() * 5);
+  topic = arr[index];
+  console.log(topic)
+  return redditApi(topic);
+}
 
 // Reddit API Request
 function redditApi(topic) {
-  let chosenTopic = topic
   let redditApi = 'https://www.reddit.com/r/';
   let oReq = new XMLHttpRequest();
   oReq.addEventListener('load', function () {
-  let fetched = JSON.parse(this.responseText);
-  let arr = fetched.data.children;
-  cardBuilder(arr, parentElement)
+    let fetched = JSON.parse(this.responseText);
+    let arr = fetched.data.children;
+    cardBuilder(arr, parentElement)
   });
-  oReq.open('GET', redditApi + chosenTopic + '.json');
+  oReq.open('GET', redditApi + topic + '.json');
   oReq.send();
 }
 
@@ -33,7 +55,7 @@ function cardBuilder(arr, parentElement) {
   cardContainer.className = 'cardContainer';
 
   arr.forEach(function (element, index, array) {
-
+    //console.log(element.data);
     let content = document.createElement('div');
     content.className = 'content';
     document.getElementById('contentContainer');
@@ -44,7 +66,7 @@ function cardBuilder(arr, parentElement) {
     image.onerror = function () {
       image.src = 'https://www.sciencedaily.com/images/2017/08/170809142044_1_540x360.jpg';
     }
-    image.src = element.data.url;
+    image.src = element.data.thumbnail;
 
     let title = document.createElement('div');
     title.className = 'contentTitle';
@@ -76,5 +98,3 @@ function cardBuilder(arr, parentElement) {
   parentElement.innerHTML = '';
   parentElement.appendChild(cardContainer);
 }
-
-
